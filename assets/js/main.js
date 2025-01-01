559d0570091e757abc5f05431f28795d56adccb7
danieltsega <danieltsega6658@gmail.com>
2025-01-01 12:40:38 +0300

    feat: add the logic to toggle between the menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,36 @@
 /*=============== LOADER ===============*/
 
-
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
+/*======= Menu Show =======*/
+/** Validate if constant exixts */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/** Menu Hidden */
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
-
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
 
 /*=============== MIXITUP FILTER PRODUCTS ===============*/
 
+/* Default filter products */
 
-/* Default filter products */ 
+/* Link active products */
 
-
-/* Link active products */ 
-
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
